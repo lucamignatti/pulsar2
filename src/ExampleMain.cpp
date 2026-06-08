f223d2ac09ee718d9a07fb425cd9cbdacebae04b
Luca Mignatti <lucamignatti@icloud.com>
2026-06-07 21:59:38 -0400

    disable render

--- src/ExampleMain.cpp
+++ src/ExampleMain.cpp
@@ -341,8 +341,8 @@ int main(int argc, char* argv[]) {
 	cfg.ppo.gcrlCritic.addLayerNorm = false;
 	cfg.ppo.sorsReward.addLayerNorm = addLayerNorm;
 
-	cfg.sendMetrics = false; // Send metrics
-	cfg.renderMode = true; // Don't render
+	cfg.sendMetrics = true; // Send metrics
+	cfg.renderMode = false; // Don't render
 	cfg.ppo.deterministic = true;
 
 	// Make the learner with the environment creation function and the config we just made
