0a6f5b967601fde3c615299f5e99694bab748dbd
Luca Mignatti <lucamignatti@icloud.com>
2026-07-02 18:53:48 -0400

    Add Torch discovery fallback and CUDA probe hints

--- src/ExampleMain.cpp
+++ src/ExampleMain.cpp
@@ -4,8 +4,8 @@
 #include <RLGymCPP/Rewards/ZeroSumReward.h>
 #include <RLGymCPP/TerminalConditions/NoTouchCondition.h>
 #include <RLGymCPP/TerminalConditions/GoalScoreCondition.h>
-#include <RLGymCPP/OBSBuilders/DefaultObs.h>
-#include <RLGymCPP/OBSBuilders/AdvancedObs.h>
+#include <RLGymCPP/ObsBuilders/DefaultObs.h>
+#include <RLGymCPP/ObsBuilders/AdvancedObs.h>
 #include <RLGymCPP/StateSetters/KickoffState.h>
 #include <RLGymCPP/StateSetters/RandomState.h>
 #include <RLGymCPP/StateSetters/BallNearCarState.h>
